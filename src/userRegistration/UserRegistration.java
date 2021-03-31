992681f4f107066506d8091e95ff47fa7f4cd1d1
dinnu247 <dineshkumar.icon@gmail.com>
2021-03-31 20:31:53 +0530

    ADDED:validateFirstName Method

--- src/userRegistration/UserRegistration.java
+++ src/userRegistration/UserRegistration.java
@@ -1,8 +1,30 @@
 package userRegistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    static Scanner sc = new Scanner(System.in);
+
+    private String getFirstName() {
+        System.out.println("Enter Your First Name Of First Letter In Capital ");
+        return sc.next();
+    }
+    private void validateFirstName(){
+        boolean test = Pattern.matches("[A-Z][a-z]{2,}",getFirstName());
+        if (test) {
+            System.out.println("Your Entry Is valid");
+        }else {
+            System.out.println("Please Enter Minimum Of Three Characters");
+        }
+
+     }
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
+        UserRegistration user = new UserRegistration();
+        user.validateFirstName();
 
     }
+
+
 }
